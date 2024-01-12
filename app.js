ced4aa5ad1a5e9fd7f8e88b86be98476980b034a
Rodrigo Paez <rodripari2009@gmail.com>
2024-01-12 18:29:39 +0100

    Portfolio

---
+++ app.js
@@ -0,0 +1,20 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+        else{
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+const nav =document.querySelector('.nav');
+
+window.addEventListener('scroll',function(){
+    nav.classList.toggle('active', window.scrollY >0)
+})
